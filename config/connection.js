1979331a05d2bcebd47e8fd96a28e2a0664f2d5e
yingvang19 <yingvang19@hotmail.com>
2019-07-22 23:31:46 -0500

    database setup

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  user: "b35d570e48c2a7",
+  password: "540324e1",
+  database: "heroku_1adb1b7804b6992"
 });
 
 // Make connection.
