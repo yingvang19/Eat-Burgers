4d8085311a7e1f13ffa8165c86c6ab493fb31578
yingvang19 <yingvang19@hotmail.com>
2019-07-24 20:54:00 -0500

    update jawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "kq2npu2ju4peoy3s",
-  password: "r8mz1i0c1psuf274",
-  database: "kcikby46oa29k7ph"
-});
+if(process.env.JAWSDB_URL){
+
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else{
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
